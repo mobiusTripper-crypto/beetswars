aa21d6edd4418568cb2b20f79436ae4991a65661
stringer5k <spinkbickle@gmail.com>
2022-02-18 15:19:05 -0500

    flex layout

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -6,6 +6,7 @@ import useGetData from "hooks/useGetData";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
 
 const PageContent: FC = () => {
   // const service = useBribeDataService();
@@ -14,62 +15,80 @@ const PageContent: FC = () => {
   const getData = useGetData();
 
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        px: 5,
+      }}
+    >
       {getData.status === "loading" && <div>Loading...</div>}
       {getData.status === "loaded" &&
         getData.payload.map((data, index: number) => (
-          <Card sx={{ maxWidth: 480, margin: 5 }} key={index}>
-            <CardHeader
-              title={data.poolName}
-              subheader={"$ / fBEETS: " + data.valuePerVote.toFixed(5)}
-            />
-            <CardContent>
-              <p>
-                <strong>Reward: </strong>
-                {data.rewardDescription}{" "}
-              </p>
-              <p>
-                Reward Value:{" "}
-                {"$" +
-                  data.rewardValue.toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-              </p>
-              <p>
-                Percent Above Threshhold:{" "}
-                {data.percentAboveThreshold.toFixed(2) + "%"}
-              </p>
-              <p>
-                Percent Value:{" "}
-                {"$" +
-                  data.percentValue.toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-              </p>
-              <p>
-                Overall Value:{" "}
-                {"$" +
-                  data.overallValue.toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-              </p>
-              <p>
-                Vote Total:{" "}
-                {data.voteTotal.toLocaleString(undefined, {
-                  minimumFractionDigits: 0,
-                  maximumFractionDigits: 0,
-                }) +
-                  " (" +
-                  data.votePercentage.toFixed(2) +
-                  "% )"}
-              </p>
-            </CardContent>
-          </Card>
+          <Box
+            key={index}
+            sx={{
+              flex: {
+                lg: "0 1 calc(33% - 1em)",
+                md: "0 1 calc(50% - 1em)",
+                sm: "0 1 100%",
+              },
+            }}
+          >
+            <Card sx={{ margin: 5 }} key={index}>
+              <CardHeader
+                title={data.poolName}
+                subheader={"$ / fBEETS: " + data.valuePerVote.toFixed(5)}
+              />
+              <CardContent>
+                <p>
+                  <strong>Reward: </strong>
+                  {data.rewardDescription}{" "}
+                </p>
+                <p>
+                  Reward Value:{" "}
+                  {"$" +
+                    data.rewardValue.toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                </p>
+                <p>
+                  Percent Above Threshhold:{" "}
+                  {data.percentAboveThreshold.toFixed(2) + "%"}
+                </p>
+                <p>
+                  Percent Value:{" "}
+                  {"$" +
+                    data.percentValue.toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                </p>
+                <p>
+                  Overall Value:{" "}
+                  {"$" +
+                    data.overallValue.toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                </p>
+                <p>
+                  Vote Total:{" "}
+                  {data.voteTotal.toLocaleString(undefined, {
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  }) +
+                    " (" +
+                    data.votePercentage.toFixed(2) +
+                    "% )"}
+                </p>
+              </CardContent>
+            </Card>
+          </Box>
         ))}
-    </div>
+    </Box>
   );
 };
 
