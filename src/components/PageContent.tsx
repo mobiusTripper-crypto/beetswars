96255cd8186b86f43f80e0c7c7ae67c869a10249
stringer5k <spinkbickle@gmail.com>
2022-02-26 17:54:41 -0500

    Update PageContent.tsx

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -17,11 +17,28 @@ const PageContent: FC = () => {
 
   return (
     <div>
-      <Typography variant="h2" fontWeight="700" align="center">
-        <Box sx={{ display: "inline", color: "#4BE39C" }}>BEETS WARS</Box>
-        {" - "}
-        <Box sx={{ display: "inline", color: "#ED1200" }}>ROI Dashboard</Box>
-      </Typography>
+      <Box
+        sx={{
+          display: "grid",
+          gridgap: 2,
+          gridTemplateColumns: "200px minmax(200px, 1fr) 200px",
+        }}
+      >
+        <div></div>
+        <Typography variant="h2" fontWeight="700" align="center">
+          <Box sx={{ display: "inline", color: "#4BE39C" }}>BEETS WARS</Box>
+          {" - "}
+          <Box sx={{ display: "inline", color: "#ED1200" }}>ROI Dashboard</Box>
+        </Typography>
+        <Box sx={{ display: "block", align: "right" }}>
+          <Typography variant="caption" align="right">
+            App:{process.env.REACT_APP_VERSION}
+          </Typography>
+          <Typography variant="caption" align="right">
+            B
+          </Typography>
+        </Box>
+      </Box>
       <Typography variant="body1" align="center">
         This website is in BETA TESTING. This is 3rd party service independent
         of BeethovenX and please do your own research. This is not investment
